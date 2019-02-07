76f92bdcd3be709ea7b9c7084a09d783ec7cb465
Jeevan Karloss <ajkarloss@gmail.com>
2019-02-07 22:42:21 +0100

    Cleaning the reads - removing adapters and PhiX

---
+++ bin/Clean_Raw_Reads.py
@@ -0,0 +1,40 @@
+from os import listdir
+import sys
+import re
+import os
+import configparser
+
+# User defined modules
+import Sub_Functions as SF
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+file_location = config['BASIC']['FASTQ_FILES']
+phix_location = config['BASIC']['PHIX_GENOME']
+adapter_location = config['BASIC']['ADAPTER_FILE']
+
+# Call to import the Paired end files
+fastq_files = SF.list_paird_end_files(file_location)
+
+bbduck = "/usit/abel/u1/jeevka/FHI/Vibrio_Project_2018/Vibrio_vulnificus/Pipelines/bbmap/bbduk.sh"
+
+# Create output folder
+if not os.path.exists("Trimmed_Fastq_Files"):
+    os.makedirs("Trimmed_Fastq_Files")
+
+for i in range(0,len(fastq_files),2):
+    
+    # Inout Files
+    INF1 = file_location + fastq_files[i]
+    INF2 = file_location + fastq_files[i+1]
+    
+    # Output Files
+    OF1 = "Trimmed_Fastq_Files/" + fastq_files[i].split(".")[0] + "_Trimmed.fastq" 
+    OF2 = "Trimmed_Fastq_Files/" + fastq_files[i+1].split(".")[0] + "_Trimmed.fastq"
+    log_file = "Trimmed_Fastq_Files/" + fastq_files[i] + ".log"
+    
+    print ("Executing bbduk for..", fastq_files[i],"\t",fastq_files[i+1],"\n")
+    
+    exec_bbduck = bbduck + " threads=5 ref=" + phix_location + "," + adapter_location + " in1=" + INF1 + " in2=" + INF2 + " out=" + OF1 + " out2=" + OF2 + " k=31 ktrim=r mink=11 hdist=1 tbo=f tpe=f qtrim=r trimq=15 maq=15 minlen=36 forcetrimright=149 stats=stats.txt &> " + log_file
+    os.system(exec_bbduck)
