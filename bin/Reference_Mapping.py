e0438a6f95ce801d8d64a08915f7401b6b786147
Jeevan Karloss <ajkarloss@gmail.com>
2019-02-07 22:43:00 +0100

    Mapping the reads to reference and create SAM and BAM files

---
+++ bin/Reference_Mapping.py
@@ -0,0 +1,27 @@
+import re
+import sys
+import configparser
+import os
+
+# User defined modules
+import Sub_Functions as SF
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+file_location = config['BASIC']['FASTQ_FILES']
+ref_genome = config['BASIC']['REF_GENOME']
+
+fastq_files = SF.list_paired_end_files_with_pattern(file_location,"*.fastq")
+
+# Create output folder
+if not os.path.exists("Reference_Mapped_Files"):
+    os.makedirs("Reference_Mapped_Files")
+
+for i in range(0,len(fastq_files),2):
+    # Input Files
+    INF1 = file_location + fastq_files[i]
+    INF2 = file_location + fastq_files[i+1]
+    OF = "Reference_Mapped_Files/" + fastq_files[i].split(".")[0]
+    execute_bowtie2 = "bowtie2 --sensitive -p 30 -x " + ref_genome + " -1 " + INF1 + "-2 " + INF2 + " -S " + OF + ".sam"
+    os.system(execute_bowtie2)
